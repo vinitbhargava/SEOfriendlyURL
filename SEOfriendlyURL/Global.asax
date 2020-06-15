f0d2d67462a1c3eceef93a04b4108589f54160c3
Vinit Bhargava <er.bhargava.vinit@gmail.com>
2020-06-15 09:10:28 +0530

    Generation SEO Friendly URLs in MVC 5

---
+++ SEOfriendlyURL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SEOfriendlyURL.MvcApplication" Language="C#" %>
---
+++ SEOfriendlyURL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEOfriendlyURL.Startup))]
+namespace SEOfriendlyURL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
